cffd2ebf76c0c6c5c60f85ee3901ccd3e926d4e1
David Daly <dalyd14@gmail.com>
2020-11-01 15:06:19 -0500

    nevermind queryselector is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var formEl = document.getElementById("task-form");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function (event) {
